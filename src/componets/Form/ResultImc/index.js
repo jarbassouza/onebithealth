4833a1ff0bb323933de74905779c8b4323818507
Jarbas Pereira de Souza <jpsdelarue@gmail.com>
2025-01-04 22:53:44 -0300

    Atualizado em 04-12-2024 as 22h53

--- src/componets/Form/ResultImc/index.js
+++ src/componets/Form/ResultImc/index.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Share } from "react-native";
 import styles from "./style";
 
 export default function ResultImc(props) {
-  return(
+
+  const onShare = async () => {
+    const result = await Share.share({
+      message: `Meu IMC hoje é: ${props.resultImc}`
+    });
+  };  
+  
+  return (
+    
     <View style={styles.contextImc}>
+
+      <View style={styles.boxSharedButton}>
+
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.numberImc}>{props.resultImc}</Text>
+    
+
+      {props.resultImc != null ?
+  
+      <TouchableOpacity onPress={onShare} style={styles.buttonShare}>
+        <Text style={styles.sharedText}>Compartilhar?</Text>
+      </TouchableOpacity>
+      :
+      <View/>
+      }
+      </View>
+     
     </View>
+    
   );
-}  
+ 
+};
--- src/componets/Form/ResultImc/style.js
+++ src/componets/Form/ResultImc/style.js
@@ -20,6 +20,26 @@ const styles = StyleSheet.create({
         color: "#ff0043",
         fontWeight: "bold",
     },
-});
+    buttonShare: {
+        backgroundColor: "#FF0043",
+        borderRadius: 50,
+        padding: 10,
+        marginTop: 10,
+    },
+
+    shared: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 10,
+    },
+
+    sharedText: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
+    },
+     
+}); 
 
 export default styles;
